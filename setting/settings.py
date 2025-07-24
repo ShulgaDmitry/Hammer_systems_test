6067623768b9ac4bbc75dc88de3cfac941d32cd5
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2025-07-24 23:34:55 +0300

    Project upload third

--- setting/settings.py
+++ setting/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = Path(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
