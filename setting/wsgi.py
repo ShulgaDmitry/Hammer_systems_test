383b7fbe7bbd4a1c2abcf293bc31414b2dc1ae02
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2025-07-25 00:16:58 +0300

    Project upload forth

--- setting/wsgi.py
+++ setting/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setting.settings')
+#
+# application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
+
+import os
+import sys
+
+path = '/home/dmitry161992/Hammer_systems_test'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setting.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
