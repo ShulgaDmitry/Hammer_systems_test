de68040d5ecc8d3c09f6baaaefcde0b3de8326fa
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2025-07-24 21:58:57 +0300

    Project upload

--- setting/settings.py
+++ setting/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,15 +75,23 @@ WSGI_APPLICATION = 'setting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Hammerdb',
+#         'USER': 'postgres',
+#         'PASSWORD': '1111',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Hammerdb',
-        'USER': 'postgres',
-        'PASSWORD': '1111',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(
+        'postgresql://postgres:BJwLjNjNhMzGXqhEqdEsBJtuVZWmSvsV@mainline.proxy.rlwy.net:52160/railway',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
